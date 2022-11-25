b0830d6c377d271f180dadb526360ca2e1851358
sonurborg <j.medina11@ufromail.cl>
2022-11-25 04:55:46 -0300

    avance en la gui del visualizador

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import { useState } from 'react'
-import boxi from './assets/boxi.png'
-import './App.css'
+import { useState } from "react";
+import boxi from "./assets/boxi.png";
+import "./App.css";
+
+import Impress, { Step, Slide } from "impress.js-react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <Impress>
+        <Slide x={50} y={300} z={35}>
+          Hello World
+        </Slide>
+        <Step x={50} y={30} z={35}>
+          Test
+        </Step>
+        <img x={40} rotateX={45} src="./pic.png" />
+      </Impress>
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="./src/test/reader.html" target="_blank">
+        <a href="./src/test/page1.html" target="_blank">
           <img src={boxi} className="logo react" alt="React logo" />
         </a>
       </div>
@@ -28,7 +40,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
