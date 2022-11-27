f431a0071e44245d621a6cbcd6fd5c55979ce154
sonurborg <j.medina11@ufromail.cl>
2022-11-27 06:10:43 -0300

    Overview button fix, page turning now works, page selector in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,78 @@
-import { useState } from "react";
+import { useState, useLayoutEffect } from "react";
 import boxi from "./assets/boxi.png";
 import "./App.css";
-
-import Impress, { Step, Slide } from "impress.js-react";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  useLayoutEffect(() => {
+    const script = document.createElement("script");
+    script.innerHTML = "impress().init();";
+    script.src = "/impress.js";
+    script.async = true;
+    document.body.appendChild(script);
+  });
+
   return (
     <div className="App">
-      <Impress>
-        <Slide x={50} y={300} z={35}>
-          Hello World
-        </Slide>
-        <Step x={50} y={30} z={35}>
-          Test
-        </Step>
-        <img x={40} rotateX={45} src="./pic.png" />
-      </Impress>
+      {/* MEMEMEMEME */}
+      <div
+        id="impress-toolbar"
+        style={{ position: "absolute", zIndex: 1 }}
+      ></div>
+
+      <div id="impress" data-transition-duration="250">
+        <div id="overview" class="step active" data-x="0" data-y="0" data-z="0">
+          <img src="/1.jpg" />
+        </div>
+        <div
+          id="step-1"
+          class="step future"
+          data-x="0"
+          data-y="-250"
+          data-z="1"
+          data-scale="0.5"
+        >
+          <div
+            id="step-1-child"
+            style={{
+              width: "1500px",
+              height: "1000px",
+              border: "3000px solid #303030",
+              opacity: 0.99,
+            }}
+          ></div>
+        </div>
+
+        <div
+          id="step-2"
+          class="step future"
+          data-x="168"
+          data-y="112"
+          data-z="2"
+          data-scale="0.25"
+        >
+          <div
+            id="step-2-child"
+            style={{
+              width: "1200px",
+              height: "850px",
+              border: "3000px solid #303030",
+              opacity: 0.99,
+            }}
+          ></div>
+        </div>
+      </div>
+      {/* MEMEMEMEME */}
 
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="./src/test/page1.html" target="_blank">
+        <a
+          href="http://localhost:5173/src/test/manga/hitoribocchi-no-oo-seikatsu/chapter1_readerTest/reader.html"
+          target="_blank"
+        >
           <img src={boxi} className="logo react" alt="React logo" />
         </a>
       </div>
