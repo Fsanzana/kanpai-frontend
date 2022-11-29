9b4c0a65888ec5d03ef27f578c56d391a06db334
JAVIER ALEJANDRO MEDINA GOMEZ~ <j.medina11@ufromail.cl>
2022-11-29 15:52:36 -0300

    continuing work on custom class, page switching achieved, working on stepping the frame

--- src/test/class.mangaReader.js
+++ src/test/class.mangaReader.js
@@ -32,6 +32,9 @@ class mangaReader {
     //SET GUI
     this.setGui();
 
+    //SET DEFAULT EMPTY FRAME
+    this.setDiv();
+
     //SET STEPS
     this.retrieveJson();
   }
@@ -53,20 +56,17 @@ class mangaReader {
             if (parseInt(value.page) === parseInt(reader.currentPage)) {
               for (var i = 0; i < value.panels.length; i++) {
                 if (value.panels[i].id === "overview") {
-                  reader.stepValues[i] = [0, 0, 0];
+                  reader.stepValues[0] = [0 , 0 , 0 , 0 , 0 , 0];
                   reader.setOverview(reader.src + value.panels[i].src);
                 } else {
-                  reader.stepValues[i + 1] = [
-                    value.panels[i + 1].id.split("-").pop(),
-                    value.panels[i + 1].dataX,
-                    value.panels[i + 1].dataY,
+                  reader.stepValues[i+1] = [
+                    value.panels[i].id.split("-").pop(),
+                    value.panels[i].dataX,
+                    value.panels[i].dataY,
+                    value.panels[i].width,
+                    value.panels[i].height,
+                    value.panels[i].dataScale
                   ];
-                  reader.newDiv(
-                    reader.stepValues[i + 1],
-                    value.panels[i + 1].width,
-                    value.panels[i + 1].height,
-                    value.panels[i + 1].scale
-                  );
                 }
               }
             }
@@ -79,26 +79,28 @@ class mangaReader {
 
   //NEXT SLIDE / PAGE
   nextButtonFunc() {
-    console.log("next");
+    //console.log("next");
     if (this.overviewMode) {
       this.nextPage();
     } else {
       if (this.currentStep === this.steps[this.currentPage]) {
-        this.currentStep = 0;
+        //console.log('0');
+        this.nextPage();
       } else {
         if (this.currentStep + 1 < this.steps[this.currentPage] - 1) {
+          //console.log('1');
+          this.stepFrame();
           this.currentStep++;
         } else {
           this.nextPage();
         }
       }
     }
-    console.log(this.stepValues);
     this.updateStyle(this.stepValues[this.currentStep]);
-    console.log(this.currentStep);
   }
 
   nextPage() {
+    //console.log('next page');
     if (this.currentPage + 1 < this.chapters[this.currentChapter] + 1) {
       this.currentPage++;
       //fixing this, finalize pls
@@ -128,6 +130,15 @@ class mangaReader {
       document.getElementById("pageTextField").value = this.currentPage;
     } else {
       console.log("NO MORE CHAPTERS TO LOAD, BACK TO MANGA PAGE");
+    }
+  }
+
+  // MOVE THE FOCUS FRAME
+  stepFrame(){
+    for(var i=0;i<this.steps[parseInt(this.currentPage)];i++){
+      var tempId='step-'+(parseInt(i)+1);
+      console.log(document.getElementById(tempId));
+      document.getElementById(tempId).style.display;
     }
   }
 
@@ -167,18 +178,17 @@ class mangaReader {
     }
   }
 
-  newDiv(step, width, height, scale) {
+  setDiv() {
+    var step= [0,0,0,0,0,0];
     var temp =
-      '<div id="step-' +
-      step[0] +
-      '" data-x="' +
+      '<div id="frame" data-x="' +
       step[1] +
       '" data-y="' +
       step[2] +
       '" data-z="' +
       step[0] +
       '" data-scale="' +
-      scale +
+      step[5] +
       '" data-rel-position="absolute" style="position: absolute; transform: translate(-50%, -50%) translate3d(' +
       step[1] +
       "px, " +
@@ -186,28 +196,52 @@ class mangaReader {
       "px, " +
       step[0] +
       "px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scale(" +
-      scale +
-      '); transform-style: preserve-3d;"><div id="step-' +
-      step[0] +
-      '-child" style="width: ' +
-      width +
+      step[5] +
+      '); transform-style: preserve-3d;"><div id="frame-child" style="width: ' +
+      step[3] +
       "px; height: " +
-      height +
+      step[4] +
       'px; border: 3000px solid #303030; opacity: 0.99;"></div></div>';
     document.getElementById("readerStyle").innerHTML += temp;
   }
 
+  changeFrame() {
+    document.getElementById("frame").dataX=step[1];
+    document.getElementById("frame").dataY=step[2];
+    document.getElementById("frame").dataZ=step[0];
+    document.getElementById("frame").scale=step[5];
+    document.getElementById("frame").style.transform='translate(-50%, -50%) translate3d(' +
+    step[1] +
+    "px, " +
+    step[2] +
+    "px, " +
+    step[0] +
+    "px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scale(" +
+    step[5] +
+    ')';
+
+    document.getElementById("frame-child").style.width=step[3];
+    document.getElementById("frame-child").style.width=step[4];
+  }
+
   setOverview(src) {
+    var pageSize = (screen.height/3)-screen.height/25;
     var temp =
-      '<div id="overview" class="active" data-x="0" data-y="0" data-z="0" data-rotate-x="0" data-rotate-y="0" data-rotate-z="0" data-rotate-order="xyz" data-rel-position="absolute" data-rel-x="0" data-rel-y="0" data-rel-z="0" data-rel-rotate-x="0" data-rel-rotate-y="0" data-rel-rotate-z="0" style="position: absolute; transform: translate(-50%, -50%) translate3d(0px, 0px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scale(1); transform-style: preserve-3d;"><img src="' +
+      '<div id="overview" class="active" data-x="0" data-y="0" data-z="0" data-rotate-x="0" data-rotate-y="0" data-rotate-z="0" data-rotate-order="xyz" data-rel-position="absolute" data-rel-x="0" data-rel-y="0" data-rel-z="0" data-rel-rotate-x="0" data-rel-rotate-y="0" data-rel-rotate-z="0" style="position: absolute; transform: translate(-50%, -50%) translate3d(0px, 0px, 0px) rotateX(0deg) rotateY(0deg) rotateZ(0deg) scale(1); transform-style: preserve-3d;"><img id="pageImage" src="' +
       src +
-      '" style="width: 666.66px; height: 1000px"></div>';
+      '" style="width: '+
+      (pageSize*2)+
+      'px; height: '+
+      (pageSize*3)+
+      'px"></div>';
     document.getElementById("readerStyle").innerHTML += temp;
   }
 
-  setGui() {
-    //pun onkey down
+  changeOverview(src) {
+    document.getElementById("pageImage").src=src;
+  }
 
+  setGui() {
     var temp =
       '<button id="overviewSwitch" onclick="reader.overviewSwitchFunc()" title="Switch view" style="position: absolute;margin-left: 10px;margin-bottom: 10px;left: 0px;bottom: 0px;border-radius: 2rem;background-color: #b63333;border: none;color: #303030;padding: 0.35rem 0.5rem;background-position: center;background-size: contain;cursor: pointer;"><img id="overviewIcon" src="../assets/overview.png" height="30rem"/></button><button id="nextButton" onclick="reader.nextButtonFunc()" title="Next"></button><button id="prevButton" onclick="reader.prevButtonFunc()" title="Previous" ></button><div id="pageInput" style="cursor: pointer;display: flex;height: fit-content;position: absolute;align-items: center;margin-right: 10px;margin-bottom: 10px;bottom:0px;right: 0px;border-radius: 2rem;background-color: #b63333;border: none;padding: 0rem 0.4rem;width: 3.2rem;background-position: center;background-size: contain;font-size: 1.25rem;text-align:left"><input id="pageTextField" type="text" onkeypress="return reader.onlyNumberKey(event)" onkeydown="reader.pageSelector(this)" maxlength="3" value="0" max="999" min="0" style="color:#303030;width: 1.15rem;align-items: center;background-color:transparent;border: none;font-size: 1.25rem;text-align:right" title="Choose page">/<a id="pageAmount" title="Current page" ></a></div>';
     document.getElementById("readerGui").innerHTML = temp;
@@ -222,7 +256,9 @@ class mangaReader {
       "px," +
       -parseInt(values[0]) +
       "px)";
+      var scale=(1/parseInt(values[3]));
     document.getElementById("readerStyle").style.transform = temp;
+    //document.getElementById("mangaReader").style.transform='scale('+(1/parseInt(values[3]))+')';
   }
 
   onlyNumberKey(evt) {
