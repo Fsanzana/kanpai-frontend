876370e98088a7ea9ee7cb576d3b9055bdee1738
Bayron Muñoz <b.munoz15@ufromail.cl>
2022-12-13 01:14:07 -0300

    feat: work with backend's json

--- src/pages/Chapters.jsx
+++ src/pages/Chapters.jsx
@@ -6,48 +6,71 @@ import ChapterMenu from "../components/menu/chapterMenu";
 import DescCard from "../components/card/descCard";
 import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 import Box from "@mui/material/Box";
-import { Container } from "@material-ui/core";
+import { Container, Paper } from "@material-ui/core";
+import { getMangabyName } from "../service/MangaService";
+
+
+
 function Chapters(props) {
-  const file = "/src/test" + props.name + "/chapters.json";
-  const [data, setData] = useState([]);
-  const fetchData = () => {
-    fetch(file)
-      .then((response) => response.json())
-      .then((actualData) => {
-        setData(actualData);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const { data, isLoading, error } = getMangabyName(props.name);
+
+  if (isLoading) {
+    return "meme";
+  }
+  if (error) {
+    return "meme2";
+  }
+
   return (
     <div className="Chapters">
-      <Box component="main" sx={{ flexGrow: 1, p: 0, marginTop: "2%" }}>
-        {data?.map((item, index) => (
-          <Grid2 container justifyContent={"space-around"} key={index}>
-            <Grid2 xs="auto">
-              <TimgCard
-                img={item.img}
-                state={item.state}
-                publication={item.publication}
-                tags={item.tags}
-              />
-            </Grid2>
-            <Grid2 xs="auto" sx={{ width: "70%" }}>
-              <Container direction="column">
-                <DescCard title={item.title} desc={item.desc} />
-                <ChapterMenu
-                  chapters={item.chapters}
-                  name={props.name.replace("/", "").replace("manga", "")}
-                />
-              </Container>
-            </Grid2>
+      <Paper
+        elevation={3}
+        style={{
+          position: "absolute",
+          width: "100%",
+          backgroundColor: "#712222",
+          zIndex: "0",
+          opacity: "0.9",
+          top: "0",
+          height: "50vh",
+        }}
+        square
+      >
+          <img
+            src={data.manBanner}
+            style={{
+              userSelect: "none",
+              objectFit: "cover",
+              width: "100%",
+              height: "100%",
+              left: "0",
+              right: "0",
+              pointerEvents: "none",
+            }}
+          />
+      </Paper>
+      <Box component="main" sx={{ flexGrow: 1, p: 0, marginTop: "2%",zIndex: "1", position:"relative" }}>
+        <Grid2 container justifyContent={"space-around"} key={0}>
+          <Grid2 xs="auto">
+            <TimgCard
+              img={data.manThumbnail}
+              state={data.manStatus}
+              publication={data.manRealease}
+              tags={data.manGenre}
+            />
           </Grid2>
-        ))}
+          <Grid2 xs="auto" sx={{ width: "70%" }}>
+            <Container direction="column">
+              <DescCard title={data.manName} desc={data.manSynopsis} />
+              <ChapterMenu
+                chapters={data.manChapters}
+
+              />
+            </Container>
+          </Grid2>
+        </Grid2>
       </Box>
+
     </div>
   );
 }
