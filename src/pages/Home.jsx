30ee57ee20f7129e89ad389cdc7848a5e533966c
Bayron Muñoz <b.munoz15@ufromail.cl>
2022-12-13 01:13:57 -0300

    feat: work with backend's json

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,19 +4,30 @@ import Box from "@mui/material/Box";
 import Slider from "/src/components/slider/Slider.jsx";
 import { Outlet } from "react-router-dom";
 import mangaList from "/src/data/mangaList.json";
+import { getAllMangas } from "../service/CatalogService";
 
 function Home() {
+  const {data,isLoading,error} = getAllMangas();
+
+  if (isLoading) {
+    return "meme";
+  }
+  if (error) {
+    return "meme2";
+  }
+
+  
   return (
     <div className="Home">
       <Slider className="carousel" />
       <Box component="main" sx={{ flexGrow: 1, p: 0 }}>
         <Grid container justifyContent="center">
-          {mangaList.map((data, idx) => (
+          {data.map((manga) => (
             <MangaCard
-              name={data.name}
-              thumbnail={data.img}
-              link={data.href}
-              desc={data.desc}
+              name={manga.manName}
+              thumbnail={manga.manThumbnail}
+              link={"manga/"+manga.manName.toLowerCase().replaceAll(" ","-")}
+              desc={manga.manSynopsis}
             />
           ))}
         </Grid>
